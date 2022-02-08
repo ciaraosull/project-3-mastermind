13810a1372a88a904ad0982ded5135a204809751
ciaraosull <ciaraosull@gmail.com>
2022-02-08 22:00:51 +0000

    Add game_choice function for user to decide on game rules display

--- run.py
+++ run.py
@@ -4,12 +4,32 @@ def welcome_note():
     """
     Welcome note and get name input from the user.
     """
-    print("Mastermind")
-    print("Crack the Code to Win!\n")
+    print("**********************")
+    print("      Mastermind")
+    print("Crack the Code to Win!")
+    print("**********************\n")
 
-    name_str = input("Enter your name here to begin: ")
-    print(f"\nWelcome {name_str}")
-    print("Have you got what it takes to break the code?")
-
+    name_data = input("Enter your name here to begin: ")
+    print(f"\nWelcome {name_data}")
+    print("Have you got what it takes to break the code!\n")
 
 welcome_note()
+
+def game_choice():
+    rules_choice = input("For Game Rules enter R otherwise enter P to Play: ")
+    
+    if rules_choice == "R":
+        print("How to Play....")
+
+    elif rules_choice == "P":
+        print("Let's Begin!")
+
+    else:
+        print("Sorry, not a valid response\n")
+        game_choice()
+
+game_choice()
+
+
+
+

4edc3eb0f3cfe5a8f65b58a20c7928d601773a58
ciaraosull <ciaraosull@gmail.com>
2022-02-08 21:24:09 +0000

    Add function for welcome note and requesting user name

--- run.py
+++ run.py
@@ -1,3 +1,15 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+def welcome_note():
+    """
+    Welcome note and get name input from the user.
+    """
+    print("Mastermind")
+    print("Crack the Code to Win!\n")
+
+    name_str = input("Enter your name here to begin: ")
+    print(f"\nWelcome {name_str}")
+    print("Have you got what it takes to break the code?")
+
+
+welcome_note()

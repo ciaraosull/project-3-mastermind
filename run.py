ecf0af35a42ddac12dca31c6cb558309d82d6b3f
ciaraosull <ciaraosull@gmail.com>
2022-02-09 22:05:22 +0000

    Edit code to debug for best practice

--- run.py
+++ run.py
@@ -1,7 +1,5 @@
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
-
-
 def welcome_note():
     """
     Welcome note and get name input from the user.
@@ -13,12 +11,12 @@ def welcome_note():
 
     while True:
         name_data = input("Enter your name here to begin: ")
-        
 
         if validate_data(name_data):
             print(f"\nWelcome {name_data}")
             print("Have you got what it takes to break the code!\n")
             break
+
 
 def validate_data(name):
     """
@@ -43,7 +41,8 @@ def game_choice():
     Give choice to user to display rules.
     They could be a returning player.
     """
-    rules_choice = input("For Game Rules enter R otherwise enter P to Play: ").lower()
+    rules_choice = input(
+        "For Game Rules enter R otherwise enter P to Play: ").lower()
 
     if rules_choice == "r":
         print("How to Play....")
@@ -59,10 +58,12 @@ def game_choice():
 
 game_choice()
 
+
 def start_game():
+    """
+    Start game to calculate the random code
+    """
     guess = input("Please Enter your guess: ")
 
 
 start_game()
-
-
